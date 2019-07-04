db1560283df92b9f568573db17dd9e8ba5d03cda
Ghazaal <ghazal_edrisabadi@yahoo.com>
2019-07-04 21:19:35 +0430

    Implement part0

---
+++ NPDA to CFG/NPDA to CFG/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPDA_to_CFG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (StreamReader reader = new StreamReader())
+            //{
+            //    string states = reader.ReadLine().s;
+            //    string alphabets = reader.ReadLine();
+            //    string symboles = reader.ReadLine();
+            //    string initial_symbol = reader.ReadLine();
+            //    string[] relations = reader.ReadLine();
+
+            //}
+            string[] a = Console.ReadLine().Split(new char[] {','});
+            Console.WriteLine(a[1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NPDA to CFG/NPDA to CFG/Relation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPDA_to_CFG
+{
+    class Relation
+    {
+        private string State1;
+        private string InputElement;
+        private string PopElement;
+        private string PushElement;
+        private string State2;
+
+        public Relation()
+        {
+
+        }
+
+        public Relation(string s1, string input, string pop, string push, string s2)
+        {
+            this.State1 = s1;
+            this.InputElement = input;
+            this.PopElement = pop;
+            this.PushElement = push;
+            this.State2 = s2;
+
+        }
+
+        public void Make(string relation)
+        {
+            string[] initialstate = new string[1];
+            string[] finalstate = new string[1];
+
+            if (relation[0] == '-' && relation[1] == '>')
+                initialstate[0] = relation.Substring(2, 2);
+
+            if (relation[relation.Length - 3] == '*')
+                finalstate[0] = relation.Substring(relation.Length-2, 2);
+
+            string[] newrelation = relation.Split(new char[] { '-', '>', ',' });
+            State1 = newrelation[0];
+            InputElement = newrelation[1];
+            PopElement = newrelation[2];
+            PushElement = newrelation[3];
+            State2 = newrelation[4];
+
+        }
+
+
+
+    }
+}
