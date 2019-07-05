cd095119fe3d98e7414f2725a713e7207bfebfd1
Ghazaal <ghazal_edrisabadi@yahoo.com>
2019-07-05 19:46:11 +0430

    Implement part1

--- NPDA to CFG/NPDA to CFG/Program.cs
+++ NPDA to CFG/NPDA to CFG/Program.cs
@@ -19,7 +19,7 @@ namespace NPDA_to_CFG
             string[] symboles;
             string initial_symbol;
             string relations;
-
+            string[] lines = File.ReadAllLines(@"C:\Users\Asus\Desktop\Input.txt");
             using (StreamReader reader = new StreamReader(@"C:\Users\Asus\Desktop\Input.txt"))
             {
 
@@ -28,37 +28,45 @@ namespace NPDA_to_CFG
                 symboles = reader.ReadLine().Split(new char[] { ',' });
                 initial_symbol = reader.ReadLine();
 
-                relations = reader.ReadLine();
-                relation = relation.Make(relations);
-                RelationsList.Add(relation);
-            }
-
-            using (StreamWriter writer = new StreamWriter(@"C:\Users\Asus\Desktop\Output.txt)"))
-            {
-                foreach (Relation r in RelationsList)
+                int i = 4;
+                while (i < lines.Length)
                 {
-                    if (relation.PushElement.Length == 1)
-                    {
-                        {
-                            writer.WriteLine($"({relation.State1}{relation.PopElement}{relation.State2})->{relation.InputElement}");
-                        }
-                    }
-
-                    else
-                    {
-                        for (int i = 0; i < states; i++)
-                        {
-                            for (int j = 0; j < states; j++)
-                            {
-                                writer.WriteLine($"({relation.State1}{relation.PopElement}q{i})->" +
-                                    $"{relation.InputElement}({relation.State2}{relation.PushElement[0]}q{j})(q{j}{relation.PushElement[1]}q{i})");
-                            }
-
-                        }
-
-                    }
+                    relations = reader.ReadLine();
+                    if (relations == null)
+                        break;
+                    RelationsList.Add(relation.Make(relations));
+                    i++;
                 }
             }
+
+
+            StreamWriter writer = new StreamWriter(@"C:\Users\Asus\Desktop\Output.txt");
+            foreach (Relation r in RelationsList)
+            {
+                if (relation.PushElement.Length == 1)
+                {
+                    {
+                        writer.WriteLine($"({relation.State1}{relation.PopElement}{relation.State2})->{relation.InputElement}");
+                    }
+                }
+
+                else
+                {
+                    for (int i = 0; i < states; i++)
+                    {
+                        for (int j = 0; j < states; j++)
+                        {
+                            writer.WriteLine($"({relation.State1}{relation.PopElement}q{i})->" +
+                                $"{relation.InputElement}({relation.State2}{relation.PushElement[0]}q{j})(q{j}{relation.PushElement[1]}q{i})");
+                        }
+
+                    }
+
+                }
+            }
+            writer.Close();
+
         }
     }
 }
+
